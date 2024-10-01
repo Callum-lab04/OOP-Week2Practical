19a4f74b2823d0333d8718a064154d7128922a0b
Callum McConnell <McConnell-C28@ulster.ac.uk>
2024-10-01 12:00:46 +0100

    added PrintMenu method.

--- Week 2 Practical/Program.cs
+++ Week 2 Practical/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Main();
+void Main()
+{
+    PrintMenu();
+    GetOption();
+}
+static void PrintMenu()
+{
+    Console.WriteLine("Please enter a valid option from below: ");
+    Console.WriteLine("1. Hello in French");
+    Console.WriteLine("2. Hello in Spanish");
+    Console.WriteLine("3. Hello in German");
+    Console.WriteLine("4. Hello in Italian");
+    Console.WriteLine("0. Exit application");
+}
+static void GetOption()
+{
+    string option = Console.ReadLine();
+
+    try
+    {
+
+    }
+    catch
+    {
+
+    }
+}
+
